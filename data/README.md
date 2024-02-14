0360cadd2c31d2a46b9b5b197b293f30096c5329
Felix Therrien <felix.therrien@gmail.com>
2024-02-13 21:52:10 -0500

    Added paper information to most of the data and a script to find it

--- data/README.md
+++ data/README.md
@@ -2,8 +2,8 @@
 
 This data set has been constructed by Homin Shin and Rhiannon Hendley, using as sources:
 
-- [A database of experimentally measured lithium solid electrolyte conductivities evaluated with machine learning](https://www.nature.com/articles/s41524-022-00951-z)
-- [Identification of potential solid-state Li-ion conductors with semi-supervised learning](https://pubs.rsc.org/en/content/articlelanding/2023/ee/d2ee03499a)
+1. [A database of experimentally measured lithium solid electrolyte conductivities evaluated with machine learning](https://www.nature.com/articles/s41524-022-00951-z) 
+2. [Identification of potential solid-state Li-ion conductors with semi-supervised learning](https://pubs.rsc.org/en/content/articlelanding/2023/ee/d2ee03499a)
 
 ## Files
 
@@ -15,4 +15,7 @@ This data set has been constructed by Homin Shin and Rhiannon Hendley, using as 
 - `20240224.csv`: Post processing by Divya (removed all string target labels into sep_samples.csv)
 - `20240224v1.csv`: Processed string composition into elements and counts
 - `20240224v1.csv`: Log10 conversion of target column
-    
+- `20231204v1_with_paper_info.csv`: same as `20231204v1.csv` with paper information except for 56/608 entries
+- `unidentified.txt`: List of 56 entries that can't be found in either papers 
+- `laskowski_semi-fromatted.csv`: Data from the pdf SI of paper 2 above. Some of the pdf was not perfectly rendered in the csv
+- `LiIonDatabase.csv`: Data from paper 1 above, take directly from [here](http://pcwww.liv.ac.uk/~msd30/lmds/LiIonDatabase.html)
---
+++ data/get_paper_number.py
@@ -0,0 +1,69 @@
+import numpy as np
+import re
+
+def is_same_formula(formula_string1, formula_string2):
+    f1 = re.findall("([A-Za-z]{1,2})([0-9\.]*)\s*", formula_string1)
+    f2 = re.findall("([A-Za-z]{1,2})([0-9\.]*)\s*", formula_string2)
+    if len(f1) != len(f2):
+        return False
+    for elem, count in f1:
+        if (elem, count) not in f2:
+            return False
+    return True
+
+def get_paper_from_laskowski(material, laskowski_data):
+    for j, paper_material in enumerate(laskowski_data[:,0]):
+        if (material == paper_material or
+            is_same_formula(material, paper_material) or
+            paper_material + laskowski_data[min(j+1, laskowski_data.shape[0]-1),0] == material or
+            paper_material + laskowski_data[min(j+2, laskowski_data.shape[0]-1),0] == material):
+            paper_number = laskowski_data[j,-1]
+            if paper_number == "":
+                if paper_material + laskowski_data[min(j+1, laskowski_data.shape[0]-1),0] == material and laskowski_data[j+1,-1] != "":
+                    paper_number = laskowski_data[j+1,-1]
+                elif paper_material + laskowski_data[min(j+2, laskowski_data.shape[0]-1),0] == material and (laskowski_data[j+1,-1] != "" or laskowski_data[j+2,-1] != ""):
+                    if laskowski_data[j+1,-1] == "":
+                        paper_number = laskowski_data[j+2,-1]
+                    else:
+                        paper_number = laskowski_data[j+1,-1]
+                else:
+                    print(material, "Missing paper number")
+                    paper_number = "MP"
+            return "L-" + paper_number
+    return None
+
+def get_paper_from_liion(material, liion_data):
+    for j, liion_material in enumerate(liion_data[:,1]):
+        if material == liion_material or is_same_formula(material, liion_material):
+            return liion_data[j, 2]
+    return None
+    
+homin_database = "20231204v1.csv"
+laskowski_database = "laskowski_semi-fromatted.csv"
+liion_database = "LiIonDatabase.csv"
+
+homin_header = open(homin_database, "r").readline().strip().split(",")
+homin_data = np.genfromtxt(homin_database, delimiter=',', dtype=str, skip_header=1)
+laskowski_data = np.genfromtxt(laskowski_database, delimiter=',', dtype=str)
+liion_data = np.genfromtxt(liion_database, delimiter=',', dtype=str, skip_header=1)
+
+new_homin_data = np.concatenate((homin_data,np.ones((homin_data.shape[0],1))), axis=1)
+
+not_found_count = 0
+not_found = open("unidentified.txt", "w")
+
+for i, material in enumerate(homin_data[:,0]):
+    paper_info = get_paper_from_laskowski(material, laskowski_data)
+    if paper_info is None:
+        paper_info = get_paper_from_liion(material, liion_data)
+        if paper_info is None:
+            not_found_count += 1
+            print(material, file=not_found) 
+    new_homin_data[i,-1] = paper_info
+    
+not_found.close()
+            
+np.savetxt("20231204v1_with_paper_info.csv", new_homin_data, delimiter=",", fmt="%s", header=",".join(homin_header) + ", paper")
+
+print("Not found count: ", not_found_count)
+print("Total count: ", len(homin_data[:,0]))
