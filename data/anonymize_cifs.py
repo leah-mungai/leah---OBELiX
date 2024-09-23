51f7f496005bd3eb69b919866e002687b169775f
Felix Therrien <felix.therrien@gmail.com>
2024-09-23 15:31:37 -0400

    Fixed entries with partial occupencies of more than 1
    The following:
    - cifs/0o6.cif
    - cifs/ckw.cif
    - cifs/ek5.cif
    - cifs/yab.cif
    - cifs/yq2.cif
    - cifs/ywe.cif
    
    were checked manually and corrected. 5 other entries were rescaled automatically by anonymize_cifs.py.

--- data/anonymize_cifs.py
+++ data/anonymize_cifs.py
@@ -34,6 +34,24 @@ def get_occ(atoms):
         occupancies = [{s: 1.0} for s in symbols]
     return occupancies
 
+def rescale_occs(atoms, tol=1e-3):
+    occ_info = atoms.info.get('occupancy')
+    if occ_info is None:
+        return atoms
+
+    for i, occ in occ_info.items():
+        if (sum(list(occ.values())) > 1 + Composition.amount_tolerance) and (sum(list(occ.values())) < 1 + tol):
+            occ = rescale_occs_dict(occ)
+            atoms.info['occupancy'][i] = occ
+    return atoms
+            
+def rescale_occs_dict(occ):
+    total = sum(list(occ.values()))
+    print(f"Rescaling total occupancy of {total} to 1")
+    for k in occ.keys():
+        occ[k] /= total
+    return occ
+    
 def atoms_to_structure(atoms):
     structure = AseAtomsAdaptor().get_structure(atoms)
     to_remove = []
@@ -42,6 +60,8 @@ def atoms_to_structure(atoms):
         if all(np.array(list(occ.values())) == 0):
             to_remove.append(i)
         else:
+            if (sum(list(occ.values())) > 1 + Composition.amount_tolerance) and (sum(list(occ.values())) < 1 + comp_tol):
+                occ = rescale_occs(occ)
             structure[i]._species = structure[i].species.from_dict(occ)
     structure.remove_sites(to_remove)
     return structure
@@ -69,77 +89,82 @@ def is_fractional(formula):
             return True
     return False
 
-data = pd.read_excel("raw.xlsx", index_col="ID")
-folder = "new_cifs/cifs/"
-problems = dict()
-for i, row in data.iterrows():
+def process(i, folder, make_np=False):
 
-    if row["Cif ID"] != "done":
-        continue
+    with warnings.catch_warnings():
+        warnings.filterwarnings("ignore", message="crystal system .*")
+        atoms = ase_read(folder + i + ".cif", fractional_occupancies=True, format="cif")
 
-    print("Processing", i)
-
-    try:
-        with warnings.catch_warnings():
-            warnings.filterwarnings("ignore", message="crystal system .*")
-            atoms = ase_read(folder + i + ".cif", fractional_occupancies=True, format="cif")
-    except:
-        print(i, "WARNING: ASE could not read the cif")
-        problems[i] = "ASE could not read the cif"
-        #structure = Structure.from_file(folder + i + ".cif")
-        #atoms = AseAtomsAdaptor().get_atoms(structure)
-        continue
-    
-    structure = atoms_to_structure(atoms)
-
-    try:
-        structure.to("anon_cifs/" + i + ".cif", symprec=2e-3)
-    except:
-        problems[i] = "Could not write anonymized cif"
-
-    np_structure = remove_partial_occ(structure)
-
-    np_structure.to("np_cifs/" + i + ".cif", symprec=2e-3)
-    
-    #write("anon_cifs/" + i + ".cif", atoms, format="cif")
-
-    new_atoms = read("anon_cifs/" + i + ".cif", fractional_occupancies=True)
-
-    occ_info = atoms.info.get('occupancy')
-    kinds = atoms.arrays.get('spacegroup_kinds')
-
-    new_occ_info = new_atoms.info.get('occupancy')
-    new_kinds = new_atoms.arrays.get('spacegroup_kinds')
-
-    #assert SpacegroupAnalyzer(structure, symprec=2e-3).get_space_group_number() == row["Space group #"]
-
-    n_zero_occ = len([1 for occ in get_occ(atoms) if sum(list(occ.values())) == 0])
-    
-    if len(atoms) - n_zero_occ != len(new_atoms):
-        problems[i] = "Different number of atoms"
-        continue
+    atoms = rescale_occs(atoms, tol=1.1e-3)
         
-    if new_occ_info is None and occ_info is not None:
-        problems[i] = "Fractional occupancies, but none found in anonymized cif"
-        continue
-    elif occ_info is None:
-        if is_fractional(row["Reduced Composition"]):
-            problems[i] = "Fractional occupancies, but none found in original cif"
-        continue
-
+    structure = atoms_to_structure(atoms)
     
-    for site, atom in occ_info.items():
-        for a in new_occ_info.values():
-            for k, v in atom.copy().items():
-                if v == 0.0:
-                    atom.pop(k)
-            if a == atom or atom == {}:
-                break
-        else:
-            problems[i] = "Different occupancies"
-            continue
+    structure.to("anon_cifs/" + i + ".cif", symprec=2e-3)
 
-print(len(problems), "problems found")
-print(problems)
+    if make_np:
+        np_structure = remove_partial_occ(structure)
+        np_structure.to("np_cifs/" + i + ".cif", symprec=2e-3)
+
+    return atoms
+
+if __name__ == "__main__":
+    
+    data = pd.read_excel("raw.xlsx", index_col="ID")
+    folder = "new_cifs/cifs/"
+    problems = dict()
+    for i, row in data.iterrows():
+    
+        if row["Cif ID"] != "done":
+            continue
+    
+        print("Processing", i)
+
+        try:
+            atoms = process(i, folder, make_np=True)
+        except Exception as e:
+            print("PROBELM:", e)
+            problems[i] = str(e)
+    
+        if atoms is None:
+            continue
+    
+        new_atoms = read("anon_cifs/" + i + ".cif", fractional_occupancies=True)
+    
+        occ_info = atoms.info.get('occupancy')
+        kinds = atoms.arrays.get('spacegroup_kinds')
+    
+        new_occ_info = new_atoms.info.get('occupancy')
+        new_kinds = new_atoms.arrays.get('spacegroup_kinds')
+    
+        #assert SpacegroupAnalyzer(structure, symprec=2e-3).get_space_group_number() == row["Space group #"]
+    
+        n_zero_occ = len([1 for occ in get_occ(atoms) if sum(list(occ.values())) == 0])
+        
+        if len(atoms) - n_zero_occ != len(new_atoms):
+            problems[i] = "Different number of atoms"
+            continue
+            
+        if new_occ_info is None and occ_info is not None:
+            problems[i] = "Fractional occupancies, but none found in anonymized cif"
+            continue
+        elif occ_info is None:
+            if is_fractional(row["Reduced Composition"]):
+                problems[i] = "Fractional occupancies, but none found in original cif"
+            continue
+    
+        
+        for site, atom in occ_info.items():
+            for a in new_occ_info.values():
+                for k, v in atom.copy().items():
+                    if v == 0.0:
+                        atom.pop(k)
+                if a == atom or atom == {}:
+                    break
+            else:
+                problems[i] = "Different occupancies"
+                continue
+    
+    print(len(problems), "problems found")
+    print(problems)
 
         
