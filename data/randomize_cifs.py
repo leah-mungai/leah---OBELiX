95f5e869f6ac1717c24338aef7d5e1c3f9082221
Felix Therrien <felix.therrien@gmail.com>
2025-01-15 17:24:30 -0500

    randomize_cif.py adds small random noise while preserving symmetry

--- data/randomize_cifs.py
+++ data/randomize_cifs.py
@@ -7,20 +7,23 @@ from pymatgen.symmetry.analyzer import SpacegroupAnalyzer
 noise_level = 0.01
 neighbor_cutoff = 1
 
-def symmetrize(new_structure, neighbor_cutoff=neighbor_cutoff):
-    indices = []
-    for j, neighbors in enumerate(new_structure.get_all_neighbors(neighbor_cutoff)):
-        if j in indices:
-            continue
-        coords = [new_structure[j].coords]
-        for n in neighbors:
-            if n.label == new_structure[j].label:
-                indices.append(n.index)
-                coords.append(n.coords)
-        new_coords = np.array(coords).mean(axis=0)
-        new_structure[j].coords = new_coords
+input_folder = "anon_cifs/"
+output_folder = "randomized_cifs/"
 
-    new_structure.remove_sites(indices)
+def closest_shell(atom, structure, neighbor_cutoff=neighbor_cutoff):
+    dists = [n.nn_distance for n in structure.get_sites_in_sphere(atom.coords, neighbor_cutoff) if n.label == atom.label]
+    return structure.get_neighbors_in_shell(atom.coords, min(dists), 1e-5)
+
+def symmetrize(structure, new_structure, neighbor_cutoff=neighbor_cutoff):
+    for atom in structure:
+        coords = []
+        indices = []
+        for n in closest_shell(atom, new_structure, neighbor_cutoff):
+            indices.append(n.index)
+            coords.append(n.coords)
+        new_coords = np.array(coords).mean(axis=0)
+        new_structure[indices[0]].coords = new_coords
+        new_structure.remove_sites(indices[1:])
 
     # indices = []
     # for j, neighbors in enumerate(new_structure.get_all_neighbors(1)):
@@ -69,7 +72,7 @@ def add_random_noise(filename, rng):
     new_structure =  Structure.from_str(str(cif), fmt="cif")
     
     if len(new_structure) > len(structure):
-        new_structure = symmetrize(new_structure)
+        new_structure = symmetrize(structure, new_structure)
     elif len(new_structure) < len(structure):
         print("WARNING: number of atoms decreased!")
 
@@ -80,36 +83,29 @@ if __name__ == "__main__":
     rng = np.random.default_rng(seed=59603)
     
     data = pd.read_excel("raw_labeled.xlsx", index_col="ID")
-    folder = "anon_cifs/"
     problems = []
     for i, row in data.iterrows():
-    
-        if row["Cif ID"] != "done" and row["ICSD ID"] is not None:
-            continue
 
-        filename = folder + i + ".cif"
+        filename = input_folder + i + ".cif"
+        
+        if row["Cif ID"] != "done" and np.isfinite(row["ICSD ID"]):
+            shutil.copyfile(filename, output_folder + i + ".cif")
 
         print("Processing", i)
 
         structure = Structure.from_file(filename)
-
-        sym_structure = symmetrize(structure.copy())
-
-        if len(sym_structure) < len(structure):
-            breakpoint()
         
-        for attempt in range(10):
+        for attempt in range(1):
         
             new_structure = add_random_noise(filename, rng)
 
             if (len(structure) == len(new_structure) and 
                 structure.composition == new_structure.composition and
                 SpacegroupAnalyzer(structure, symprec=2e-3).get_space_group_number() == SpacegroupAnalyzer(new_structure, symprec=2e-3).get_space_group_number()):
-                new_structure.to("randomized_cifs/" + i + ".cif", symprec=2e-3)
+                new_structure.to(output_folder + i + ".cif", symprec=2e-3)
                 break
             
         else:
-            new_structure.to("randomized_cifs/" + i + ".cif", symprec=2e-3)
             print(f"PROBLEM: {i}")
             problems.append(i)
             continue
